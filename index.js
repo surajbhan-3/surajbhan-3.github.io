60368baab0586bab2c562b8e93e5c4f1913c395c
surajbhan-3 <surajbhan2boaz@gmail.com>
2023-05-15 23:47:42 +0530

    portfolio

--- index.js
+++ index.js
@@ -4,3 +4,20 @@
 GitHubCalendar(".calendar", "surajbhan-3");
 // or enable responsive functionality
 GitHubCalendar(".calendar", "surajbhan-3", { responsive: true });
+
+
+
+
+var swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+      invert: false,
+    },
+    // autoHeight: true,
+    pagination: {
+      el: '.blog-slider__pagination',
+      clickable: true,
+    }
+  });
