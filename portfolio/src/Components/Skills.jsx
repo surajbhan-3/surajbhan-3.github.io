145d1393e10d7f6a2604160e57b36fba7c65ce2c
surajbhan-3 <surajbhan2boaz@gmail.com>
2023-07-15 22:26:18 +0530

    react-portfolio

---
+++ portfolio/src/Components/Skills.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./styles/Skills.css"
+
+function Skills() {
+  return (
+    <React.Fragment>
+
+          <div className="skills-section">
+
+                      <h1>Skills</h1>
+                      <div className="skills-container">
+
+                      </div>
+          </div>
+
+    </React.Fragment>
+  )
+}
+
+export default Skills
